c7b8b92cc80d47d0daf3fd822617f91e190cf699
ewasome <e.a.kupczynska@gmail.com>
2021-03-16 23:18:54 +0100

    Add local storage management.

---
+++ src/services/todos.ts
@@ -0,0 +1,101 @@
+import LS from '../utils/localStorage';
+const STATE_KEY = 'app.state';
+
+export function getTodoLists() {
+  try {
+    const state = LS.get(STATE_KEY);
+    const lists = Object.keys(state).map(list => ({ name: state[list].name, id: list }));
+
+    return Promise.resolve(lists);
+  } catch (err) {
+    return Promise.reject(err);
+  }
+}
+
+export function addTodoList(name) {
+  try {
+    const state = LS.get(STATE_KEY);
+    const id = name.toLowerCase().trim().split(' ').join('-');
+    const newList = { id, name, items: [] };
+    LS.set(STATE_KEY, { ...state, [id]: newList });
+
+    return Promise.resolve(newList);
+  } catch (err) {
+    return Promise.reject(err);
+  }
+}
+
+export function removeTodoList(id) {
+  try {
+    const state = LS.get(STATE_KEY);
+    const newState = Object.keys(state).reduce((acc, key) => {
+      return id === key ? { ...acc } : { ...acc, [key]: state[key] }
+    }, {})
+    LS.set(STATE_KEY, newState);
+
+    return Promise.resolve(id);
+  } catch (err) {
+    return Promise.reject(err);
+  }
+}
+
+export function getTodosFromList(listId) {
+  try {
+    const state = LS.get(STATE_KEY);
+    return Promise.resolve(state[listId].items);
+  } catch (err) {
+    return Promise.reject(err);
+  }
+}
+
+export function addTodo(text, listId, assigneeId) {
+  try {
+    const state = LS.get(STATE_KEY);
+    // TODO: generate id
+    const newItem = {
+      id: text,
+      description: text,
+      assignee: assigneeId,
+      completed: false,
+    };
+    const newItems = [...state[listId].items, newItem];
+    const newState = { ...state, [listId]: { ...state[listId], items: newItems } };
+
+    LS.update(STATE_KEY, newState);
+
+    return Promise.resolve(newItem);
+  } catch (err) {
+    return Promise.reject(err);
+  }
+}
+
+
+export function toggleTodoStatus(id, listId) {
+  try {
+    const state = LS.get(STATE_KEY);
+    const items = state[listId].items;
+    const item = items.find(item => item.id === id);
+    const changedItem = { ...item, completed: !item.completed };
+    const newItems = items.map(item => (item.id === id) ? changedItem : item);
+    const newState = { ...state, [listId]: { ...state[listId], items: newItems } };
+    LS.set(STATE_KEY, newState);
+
+    return Promise.resolve(changedItem);
+  } catch (err) {
+    return Promise.reject(err);
+  }
+}
+
+export function removeTodo(id, listId) {
+  try {
+    const state = LS.get(STATE_KEY);
+    const items = state[listId].items;
+    const newItems = items.filter(item => item.id !== id);
+    const newState = { ...state, [listId]: { ...state[listId], items: newItems } };
+    LS.set(STATE_KEY, newState);
+
+    return Promise.resolve(id);
+  } catch (err) {
+    return Promise.reject(err);
+  }
+}
---
+++ src/utils/localStorage.ts
@@ -0,0 +1,20 @@
+export function getFromStorage(key) {
+  return JSON.parse(localStorage.getItem(key))
+}
+
+export function setInStorage(key, data) {
+  localStorage.setItem(key, JSON.stringify(data));
+}
+
+export function updateStorage(key, data) {
+  if (!Object.keys(data).length) {
+    localStorage.removeItem(key);
+  }
+  setInStorage(key, { ...getFromStorage(key), ...data })
+}
+
+export default {
+  get: getFromStorage,
+  set: setInStorage,
+  update: updateStorage,
+};
